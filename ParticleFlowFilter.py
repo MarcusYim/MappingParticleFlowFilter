c059215915bca45c10ec43123540ec805592c872
MarcusYim <marcusyim2004@gmail.com>
2025-01-09 16:43:29 -0800

    works

--- ParticleFlowFilter.py
+++ ParticleFlowFilter.py
@@ -1,11 +1,21 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import filterpy
+from filterpy.kalman import KalmanFilter
+from scipy.interpolate import griddata
+from matplotlib import cm
 
-prior_cov = np.array([[0.2, 1], [0.7, 0.1]])
+Np = 2
+
+rand = np.random.rand(Np, Np)
+prior_cov = np.dot(rand.T, rand)
+al_inv_prior_cov = np.linalg.inv(0.1 * prior_cov)
 inv_prior_cov = np.linalg.inv(prior_cov)
-al_inv_prior_cov = 0.01 * inv_prior_cov
-obs_cov = np.array([[0.2, 1], [1, 0.3]])
+
+rand = np.random.rand(Np, Np)
+obs_cov = np.dot(rand.T, rand)
 inv_obs_cov = np.linalg.inv(obs_cov)
-prior_mean = np.array([1, 1])
+prior_mean = np.random.rand(Np)
 
 def K(x_1, x_2):
     return np.exp(-(1/2) * np.dot(np.dot(np.transpose(x_1 - x_2), al_inv_prior_cov), x_1 - x_2))
@@ -14,22 +24,22 @@ def K_bold(x_1, x_2, xs):
     return K(x_1, x_2) * np.identity(xs.shape[1])
 
 def div_K(x_1, x_2):
-    return -np.dot(np.dot(np.transpose(al_inv_prior_cov), x_1 - x_2), K(x_1, x_2))
+    return -np.dot(np.dot(al_inv_prior_cov, x_1 - x_2), K(x_1, x_2))
 
 def H(x_1):
     return x_1
 
-def lin_H(x_1):
-    return np.identity(2)
+def lin_H(xs):
+    return np.identity(xs.shape[1])
 
-def grad_log_y_giv_x(x_1, y):
-    return np.dot(np.dot(np.transpose(lin_H(x_1)), inv_obs_cov), y - H(x_1))
+def grad_log_y_giv_x(x_1, y, xs):
+    return np.dot(np.dot(np.transpose(lin_H(xs)), inv_obs_cov), y - H(x_1))
 
 def grad_log_x(x_1):
     return np.dot(inv_prior_cov, x_1 - prior_mean)
 
-def grad_log_x_giv_y(x_1, y):
-    return grad_log_y_giv_x(x_1, y) + grad_log_x(x_1)
+def grad_log_x_giv_y(x_1, y, xs):
+    return grad_log_y_giv_x(x_1, y, xs) + grad_log_x(x_1)
 
 def ensemble_perturbation(xs, x_m):
     return np.subtract(xs, x_m)
@@ -37,18 +47,58 @@ def ensemble_perturbation(xs, x_m):
 def ensemble_mean(xs):
     return np.multiply(1 / xs.shape[0], np.sum(xs, 0))
 
-def prior_cov(xs, per):
+def app_prior_cov(xs, per):
     return np.multiply(1 / (xs.shape[0] - 1), np.dot(np.transpose(per), per))
 
 def sum_term(x, x_i, y, xs):
-    return np.add(np.dot(K_bold(x_i, x, xs), grad_log_x_giv_y(x_i, y)), div_K(x_i, x))
+    return np.add(np.dot(K_bold(x_i, x, xs), grad_log_x_giv_y(x_i, y, xs)), div_K(x_i, x))
 
 def sum_flow_eq(x, xs, y, D):
     return (1 / xs.shape[0]) * np.dot(D, np.sum([sum_term(x, xs[i], y, xs) for i in range(xs.shape[0])], 0))
 
-xs = np.array([[1, 4], [2, 1], [5,2]])
+def flow_matrix(xs, y, D):
+    return [0.05 * sum_flow_eq(xs[i], xs, y, D) for i in range(xs.shape[0])]
 
-D = prior_cov(xs, ensemble_perturbation(xs, ensemble_mean(xs)))
 
-print(sum_flow_eq(xs[2], xs, np.array([1, 1]), D))
+xs_pff = np.random.multivariate_normal(prior_mean, prior_cov, 100)
 
+xa_pff = xs_pff
+
+D = app_prior_cov(xs_pff, ensemble_perturbation(xs_pff, ensemble_mean(xs_pff)))
+
+y = 5 * np.random.rand(Np)
+
+print(f"prmean: {prior_mean}")
+print(f"prcov: {prior_cov}")
+
+print(f"pomean: {y}")
+
+flows = np.ones(Np)
+
+while np.max(flows) > 0.01:
+    flows = flow_matrix(xs_pff, y, D)
+    xs_pff = xs_pff + flows
+
+print(np.average(xs_pff, 0))
+print(xs_pff)
+
+kf = KalmanFilter(2, 2)
+kf.x = prior_mean
+kf.P = prior_cov
+kf.R = obs_cov
+kf.H = np.identity(Np)
+kf.F = np.identity(Np)
+
+kf.update(y)
+
+plt.axhline(y = y[0], color = 'r', linestyle = '-')
+plt.axvline(x = y[1], color = 'r', linestyle = '-')
+plt.scatter(xa_pff[:, 0], xa_pff[:, 1], color='g')
+plt.scatter(xs_pff[:, 0], xs_pff[:, 1], color='r')
+plt.axhline(y = kf.x[0], color = 'b', linestyle = '-')
+plt.axvline(x = kf.x[1], color = 'b', linestyle = '-')
+
+print(kf.P)
+
+plt.tight_layout()
+plt.show()
