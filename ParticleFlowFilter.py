ba9c9dff93c9abe48e69b21652cb4a4b75788367
MarcusYim <marcusyim2004@gmail.com>
2025-01-29 15:14:03 -0800

    changed convergence detection

--- ParticleFlowFilter.py
+++ ParticleFlowFilter.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import filterpy
 from filterpy.kalman import KalmanFilter
+from numpy.ma.extras import average
 from scipy.interpolate import griddata
 from matplotlib import cm
 
@@ -9,7 +10,7 @@ Np = 2
 
 rand = np.random.rand(Np, Np)
 prior_cov = np.dot(rand.T, rand)
-al_inv_prior_cov = np.linalg.inv(0.1 * prior_cov)
+al_inv_prior_cov = np.linalg.inv(0.01 * prior_cov)
 inv_prior_cov = np.linalg.inv(prior_cov)
 
 rand = np.random.rand(Np, Np)
@@ -17,6 +18,7 @@ obs_cov = np.dot(rand.T, rand)
 inv_obs_cov = np.linalg.inv(obs_cov)
 prior_mean = np.random.rand(Np)
 
+#a load of equations (have fun)
 def K(x_1, x_2):
     return np.exp(-(1/2) * np.dot(np.dot(np.transpose(x_1 - x_2), al_inv_prior_cov), x_1 - x_2))
 
@@ -57,8 +59,10 @@ def sum_flow_eq(x, xs, y, D):
     return (1 / xs.shape[0]) * np.dot(D, np.sum([sum_term(x, xs[i], y, xs) for i in range(xs.shape[0])], 0))
 
 def flow_matrix(xs, y, D):
-    return [0.05 * sum_flow_eq(xs[i], xs, y, D) for i in range(xs.shape[0])]
+    return np.array([sum_flow_eq(xs[i], xs, y, D) for i in range(xs.shape[0])])
 
+def MA(old_av, new):
+    return old_av * (19 / 20) + new / 20
 
 xs_pff = np.random.multivariate_normal(prior_mean, prior_cov, 100)
 
@@ -75,30 +79,68 @@ print(f"pomean: {y}")
 
 flows = np.ones(Np)
 
-while np.max(flows) > 0.01:
+consecutive_decrease = 0
+consecutive_increase = 0
+last_flows = np.ones(Np)
+ds = 0.05
+average_diff = 100
+moving_av = 0
+
+#haha this kind of works
+while moving_av < 0.8:
     flows = flow_matrix(xs_pff, y, D)
-    xs_pff = xs_pff + flows
+    xs_pff = xs_pff + ds * flows
 
-print(np.average(xs_pff, 0))
-print(xs_pff)
+    average_diff = np.average(np.abs(np.subtract(flows, last_flows)))
+    if 0.005 > average_diff > -0.005:
+        consecutive_decrease += 1
+        if consecutive_decrease == 10:
+            print("decrease")
+            ds *= 1.4
+            consecutive_decrease = 0
+            consecutive_increase = 0
 
+        moving_av = MA(moving_av, 1)
+    else:
+        consecutive_decrease = 0
+        moving_av = MA(moving_av, 0)
+
+    if average_diff > 0:
+        consecutive_increase += 1
+        if consecutive_increase == 10:
+            print("increase")
+            ds /= 1.4
+            consecutive_decrease = 0
+            consecutive_increase = 0
+    else:
+        consecutive_increase = 0
+
+    last_flows = flows
+
+    print(moving_av)
+
+#compare against a KF
 kf = KalmanFilter(2, 2)
 kf.x = prior_mean
 kf.P = prior_cov
 kf.R = obs_cov
 kf.H = np.identity(Np)
 kf.F = np.identity(Np)
+kf.Q = np.identity(2)
 
 kf.update(y)
+kf.predict()
+
+correct = np.random.multivariate_normal(kf.x, kf.P, 50)
 
 plt.axhline(y = y[0], color = 'r', linestyle = '-')
 plt.axvline(x = y[1], color = 'r', linestyle = '-')
 plt.scatter(xa_pff[:, 0], xa_pff[:, 1], color='g')
 plt.scatter(xs_pff[:, 0], xs_pff[:, 1], color='r')
-plt.axhline(y = kf.x[0], color = 'b', linestyle = '-')
-plt.axvline(x = kf.x[1], color = 'b', linestyle = '-')
+plt.axhline(y = prior_mean[0], color = 'g', linestyle = '-')
+plt.axvline(x = prior_mean[1], color = 'g', linestyle = '-')
+plt.scatter(correct[:, 0], correct[:, 1], color='b')
 
-print(kf.P)
 
 plt.tight_layout()
 plt.show()
